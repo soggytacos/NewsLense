cb9602db7e9f7c78f5210f1d562fa874ec25cf3a
swish <swisher66@gmail.com>
2019-12-17 18:12:06 -0600

    added the user profile page and database

--- src/main/java/org/launchcode/controllers/UserController.java
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -1,10 +1,13 @@
 package org.launchcode.controllers;
 
 import org.launchcode.models.User;
+import org.launchcode.models.data.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -14,22 +17,27 @@ import javax.validation.Valid;
 @RequestMapping("user")
 public class UserController {
 
-    @RequestMapping(value="")
-    public String index(Model model) {
+    @Autowired
+    UserDao userDao;
+
+    @RequestMapping(value="profile/{userId}", method = RequestMethod.GET)
+    public String viewProfile(Model model, @PathVariable int userId) {
+        User user = userDao.findById(userId).orElse(null);
         model.addAttribute("title", "Welcome!");
-        return "user/index";
+        model.addAttribute("user", user);
+        return "user/profile";
     }
 
     @RequestMapping(value="add", method = RequestMethod.GET)
     public String displayAddUserForm(Model model) {
         model.addAttribute("title", "Create User");
         model.addAttribute(new User());
-
         return "user/add";
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAddUserForm(Model model, String verify, @ModelAttribute @Valid User user, Errors errors) {
+    public String processAddUserForm(Model model, String verify, @ModelAttribute @Valid User user,
+                                     Errors errors) {
 
         if (errors.hasErrors()) {
             model.addAttribute("title", "Create User");
@@ -39,14 +47,16 @@ public class UserController {
         } else if (!user.getPassword().equals(verify)) {
             String passwordMessage = "Passwords do not match!";
             user.setPassword("");
-            model.addAttribute("paswordMessage", passwordMessage);
+            model.addAttribute("passwordMessage", passwordMessage);
             model.addAttribute(user);
             model.addAttribute("title", "Create User");
             return "user/add";
+
         } else {
             model.addAttribute(user);
+            userDao.save(user);
             model.addAttribute("title", "Welcome " + user.getUsername() + "!");
-            return "user/index";
+            return "user/profile";
         }
     }
 
---
+++ src/main/java/org/launchcode/models/data/UserDao.java
@@ -0,0 +1,12 @@
+package org.launchcode.models.data;
+
+import org.launchcode.models.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+
+@Repository
+@Transactional
+public interface UserDao extends CrudRepository<User, Integer> {
+}
